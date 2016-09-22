d1d91045ecf42ca721140287bc09bb8d36f0e6a8
vdelamazah <vdelamazah@gmail.com>
2016-09-21 21:28:01 -0300

    agregado el contenido en los archivos con la respuesta a la pregunta

---
+++ p3/main.js
@@ -0,0 +1,20 @@
+	var Juan = new Alumno("Juan",[3,4,5]);
+	var Pedro = new Alumno("Pedro",[6,7,6]);
+	
+	
+
+	function Alumno(nombre, notas, promedio){
+			this.nombre = nombre;
+			this.notas = notas;
+			this.promedio = function(){
+				
+				var suma = 0;
+				for(i=0; i < notas.length; i++) { 
+					suma = (suma+notas[i]);
+					promedio = suma;
+				   console.log(promedio)   
+				}
+			return (promedio/notas.length);
+
+	}
+}
