6c5473db33a2a30d62deaf070839ed046389cb1c
vdelamazah <vdelamazah@gmail.com>
2016-09-21 21:49:55 -0300

    se agregan los codigos para mover la imagen 50px a la izq o derecha con las respectivas flechas

--- p5/main.js
+++ p5/main.js
@@ -1,1 +1,19 @@
+$(document).ready(function(){
+
+			$("body").keydown(function(e) {
+			  if(e.keyCode == 39) { // left
+			    $('#foto').css({
+			    	
+			    	marginLeft: "+=50px"
+			    });
+			  }
+			  else if(e.keyCode == 37) { // right
+			    $('#foto').css({
+			    	
+			    	marginLeft: "-=50px"
+			    });
+			  }
+			});
+
+		});
 	
